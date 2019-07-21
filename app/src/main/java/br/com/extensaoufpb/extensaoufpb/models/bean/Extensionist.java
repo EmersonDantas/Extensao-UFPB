6eb0004198b5df2cc54bbb53268c2f1923a135a5
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-07-21 20:07:10 -0300

    added override methods

--- app/src/main/java/br/com/extensaoufpb/extensaoufpb/models/bean/Extensionist.java
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/models/bean/Extensionist.java
@@ -1,4 +1,26 @@
 package br.com.extensaoufpb.extensaoufpb.models.bean;
 
-public class Extensionist {
+public class Extensionist extends User implements FeedbackMananger {
+
+
+    @Override
+    public void viewSuggestions() {
+
+    }
+
+    @Override
+    public void answerSuggestion() {
+
+    }
+
+    @Override
+    public void viewQuestions() {
+
+    }
+
+    @Override
+    public void answerQuestions() {
+
+    }
+
 }
