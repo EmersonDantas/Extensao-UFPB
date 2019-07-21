d3d435ae022130ec7be426dc1c8de70fbaa25eb8
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-07-21 19:31:21 -0300

    interface added

---
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/models/bean/FeedbackMananger.java
@@ -0,0 +1,13 @@
+package br.com.extensaoufpb.extensaoufpb.models.bean;
+
+public interface FeedbackMananger {
+
+    public void viewSuggestions();
+
+    public void answerSuggestion();
+
+    public void viewQuestions();
+
+    public void answerQuestions();
+
+}
