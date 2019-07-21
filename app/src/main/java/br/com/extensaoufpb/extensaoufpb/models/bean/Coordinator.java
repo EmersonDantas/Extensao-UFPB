eaaa1b520f0e2e76b91f075a401028df120b4ad4
Emerson Dantas <emerson.ruan@dce.ufpb.br>
2019-07-21 00:24:08 -0300

    Coordinator methods signature  added

--- app/src/main/java/br/com/extensaoufpb/extensaoufpb/models/bean/Coordinator.java
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/models/bean/Coordinator.java
@@ -1,4 +1,26 @@
 package br.com.extensaoufpb.extensaoufpb.models.bean;
 
 public class Coordinator {
+
+    public void registerProject(){}
+
+    public void viewSuggestions(){}
+
+    public void answerSuggestion(){}
+
+    public void answerQuestions(){}
+
+    public void disclosureProjectSelectionProcess(){}
+
+    public void viewAllProjects(){}
+
+    public void editMyProject(){}
+
+    public void viewCandidatesEnrolledInSelection(){}
+
+    public void viewCandidateQualifications(){}
+
+    public void viewProjectParticipants(){}
+
+    public void registerActivityOrEvent(){}
 }
--- app/src/main/java/br/com/extensaoufpb/extensaoufpb/models/bean/InternalPublic.java
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/models/bean/InternalPublic.java
@@ -1,4 +1,4 @@
 package br.com.extensaoufpb.extensaoufpb.models.bean;
 
-public class Student {
+public class InternalPublic {
 }
