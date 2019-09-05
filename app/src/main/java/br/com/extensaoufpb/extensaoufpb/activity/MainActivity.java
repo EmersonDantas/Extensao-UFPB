136e295e6ea86b8ab15d29b079d9a280b6feb2fe
WaldirMarques <waldirmarquesdesouza@gmail.com>
2019-09-05 10:55:41 -0300

    criacao da tela de login

--- app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/MainActivity.java
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/MainActivity.java
@@ -1,8 +1,10 @@
 package br.com.extensaoufpb.extensaoufpb.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.com.extensaoufpb.extensaoufpb.LoginActivity;
 import br.com.extensaoufpb.extensaoufpb.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Intent startLogin = new Intent(this, LoginActivity.class);
+        startActivity(startLogin);
     }
 }
