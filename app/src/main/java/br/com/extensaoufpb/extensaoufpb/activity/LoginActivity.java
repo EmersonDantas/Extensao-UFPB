9d429963a1ca8c3d094254de88370346f705464c
WaldirMarques <waldirmarquesdesouza@gmail.com>
2019-09-05 11:02:08 -0300

    corrigindo login

--- app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/LoginActivity.java
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/LoginActivity.java
@@ -1,7 +1,9 @@
-package br.com.extensaoufpb.extensaoufpb;
+package br.com.extensaoufpb.extensaoufpb.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.extensaoufpb.extensaoufpb.R;
 
 public class LoginActivity extends AppCompatActivity {
 
--- app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/MainActivity.java
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import br.com.extensaoufpb.extensaoufpb.LoginActivity;
 import br.com.extensaoufpb.extensaoufpb.R;
 
 public class MainActivity extends AppCompatActivity {
