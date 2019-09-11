8fd96e87b6399177da2d2e1d29ec285aa3387d93
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-09-10 21:11:21 -0300

    adição da imagem de background e animação entre activity

--- app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/MainActivity.java
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/MainActivity.java
@@ -1,19 +1,33 @@
 package br.com.extensaoufpb.extensaoufpb.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
 
 import br.com.extensaoufpb.extensaoufpb.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int TIME_SLEEP = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Intent startLogin = new Intent(this, LoginActivity.class);
-        startActivity(startLogin);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent startLogin = new Intent(MainActivity.this, LoginActivity.class);
+                ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeCustomAnimation(getApplicationContext(),R.anim.fade_in , R.anim.fade_out);
+                ActivityCompat.startActivity(MainActivity.this, startLogin,activityOptionsCompat.toBundle());
+                finish();
+            }
+        },TIME_SLEEP);
+
     }
 }
