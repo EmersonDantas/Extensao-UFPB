5733bec1867b476b4adc23414aad4c7c53c6a888
WaldirMarques <waldirmarquesdesouza@gmail.com>
2019-09-15 23:35:34 -0300

    implementation of responsive components and alertDialog in the registration of academic data

--- app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/RegisterDataActivity.java
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/RegisterDataActivity.java
@@ -3,8 +3,14 @@ package br.com.extensaoufpb.extensaoufpb.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import br.com.extensaoufpb.extensaoufpb.R;
+
+import static android.app.PendingIntent.getActivity;
 
 public class RegisterDataActivity extends AppCompatActivity {
 
@@ -12,5 +18,36 @@ public class RegisterDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_data);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner01);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.selecione, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner02);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.periodo, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
+        Button buttonCadastrar =(Button) findViewById(R.id.btnCadastrar);
+
+        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+
+            }
+        });
+
+
     }
+
+    public void openDialog(){
+        DialogRegister cadastrar = new DialogRegister();
+        cadastrar.show(getSupportFragmentManager(),"Exemplo dialog");
+    }
+
 }
