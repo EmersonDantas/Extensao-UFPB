89d1765bad8c6fce0ff355014484de1dd383eb5f
WaldirMarques <waldirmarquesdesouza@gmail.com>
2019-09-16 09:49:25 -0300

    AlerteDialog fixes

--- app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/RegisterDataActivity.java
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/RegisterDataActivity.java
@@ -1,8 +1,12 @@
 package br.com.extensaoufpb.extensaoufpb.activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -46,8 +50,27 @@ public class RegisterDataActivity extends AppCompatActivity {
     }
 
     public void openDialog(){
-        DialogRegister cadastrar = new DialogRegister();
-        cadastrar.show(getSupportFragmentManager(),"Exemplo dialog");
+
+        AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+
+        alerta.setTitle(R.string.confirmacao);
+        alerta.setMessage(R.string.dialogMenssegeDadosAcademicos);
+        alerta.setCancelable(false);
+        alerta.setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int id) {
+            // FIRE ZE MISSILES!
+
+        }
+    })
+    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int id) {
+            // User cancelled the dialog
+        }
+    });
+        alerta.create();
+        alerta.show();
     }
+
+
 
 }
