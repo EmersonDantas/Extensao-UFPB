30b1620fdb486d3824f42bc04ce19c823661ca64
Emerson Dantas <emerson.ruan@dce.ufpb.br>
2019-09-20 00:52:52 -0300

    Register screen created

---
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package br.com.extensaoufpb.extensaoufpb.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.extensaoufpb.extensaoufpb.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
