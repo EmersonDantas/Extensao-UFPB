e578431876f0e6532a20ae03af275bd96229977e
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2019-09-22 23:59:32 -0300

    Ativação do click de cancelar + closeBottomSheet

--- app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/SignProjectActivity.java
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/activity/SignProjectActivity.java
@@ -8,10 +8,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import br.com.extensaoufpb.extensaoufpb.Controller.BottomSheet;
 import br.com.extensaoufpb.extensaoufpb.R;
 
 public class SignProjectActivity extends AppCompatActivity {
@@ -19,17 +21,36 @@ public class SignProjectActivity extends AppCompatActivity {
     private TextView txtNomeCoordenador, txtStatusProjeto, txtAnoProjeto, txtDescricao;
     private ImageView imgStatus;
 
+    private Button btnClose, btnRegisterProject;
+    private BottomSheet bottomSheet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_project);
 
+        init();
+
+        setAllTexts();
+
+        enableClickButtons();
+    }
+
+    private void init() {
         txtNomeCoordenador = findViewById(R.id.nomeCoordenador);
         txtStatusProjeto = findViewById(R.id.statusProjetoTxt);
         txtAnoProjeto = findViewById(R.id.anoProjeto);
         txtDescricao = findViewById(R.id.descricaoProjeto);
+
         imgStatus = findViewById(R.id.statusProjetoImg);
 
+        btnClose = findViewById(R.id.btn_close_register_project);
+        btnRegisterProject = findViewById(R.id.btn_cadastrar_projeto);
+
+        bottomSheet = BottomSheet.getInstance(getWindow().getDecorView().findViewById(android.R.id.content), null);
+    }
+
+    private void setAllTexts() {
         txtNomeCoordenador.setText("Ayla Rebouças");
         txtStatusProjeto.setText("Ativo");
         txtAnoProjeto.setText("2019");
@@ -47,32 +68,48 @@ public class SignProjectActivity extends AppCompatActivity {
                 "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing" +
                 "software like Aldus PageMaker including versions of Lorem Ipsum.");
         txtDescricao.setMovementMethod(new ScrollingMovementMethod());
+    }
 
-        findViewById(R.id.btnCadastrarProjeto).setOnClickListener(new View.OnClickListener() {
+    private void enableClickButtons() {
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                exibirConfirmacao();
+            public void onClick(View view) {
+                bottomSheet.closeBottomSheeet();
+                finish();
             }
+        });
+
+
+        btnRegisterProject.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               exibirConfirmacao();
+           }
         });
     }
 
-    public void exibirConfirmacao(){
+    public void exibirConfirmacao() {
         AlertDialog.Builder mensagem = new AlertDialog.Builder(this);
         mensagem.setTitle("Confirmação");
         mensagem.setIcon(null);
         mensagem.setMessage("Você está prestes a cadastrar o projeto X. Está de acordo?");
+
         mensagem.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Toast.makeText(SignProjectActivity.this, "Projeto cadastrado!", Toast.LENGTH_LONG).show();
             }
         });
+
         mensagem.setNegativeButton("Não", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Toast.makeText(SignProjectActivity.this, "Cadastro cancelado!", Toast.LENGTH_LONG).show();
             }
         });
+
         mensagem.show();
     }
+    
 }
