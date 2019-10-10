bdd1bd496fb39a687580e8854dde2404f428308f
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-10-10 15:26:26 -0300

    added methods login, logout and getUser

--- app/src/main/java/br/com/extensaoufpb/extensaoufpb/Controller/FacadeQuestion.java
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/Controller/FacadeQuestion.java
@@ -4,34 +4,67 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import br.com.extensaoufpb.extensaoufpb.R;
 import br.com.extensaoufpb.extensaoufpb.activity.ui.question.QuestionOfMultipleChoiceFragment;
 import br.com.extensaoufpb.extensaoufpb.activity.ui.question.QuestionOfNumberFragment;
 import br.com.extensaoufpb.extensaoufpb.activity.ui.question.QuestionOfSelectionChoiceFragment;
 import br.com.extensaoufpb.extensaoufpb.activity.ui.question.QuestionOfTextFragment;
-import br.com.extensaoufpb.extensaoufpb.R;
+import br.com.extensaoufpb.extensaoufpb.models.bean.Coordinator;
+import br.com.extensaoufpb.extensaoufpb.models.bean.ExternalPublic;
 import br.com.extensaoufpb.extensaoufpb.models.bean.Question;
+import br.com.extensaoufpb.extensaoufpb.models.bean.User;
 
 public class FacadeQuestion {
 
     private final String SELECT = "selecione", TEXT = "texto", MULTIPLE_CHOICE = "múltipla escolha", SELECION = "seleção", NUMBER = "numérico";
     private Fragment fragment;
     private int position;
+    private static User user;
+    private static final String emailCoordinator = "c1@gmail.com";
+    private static final String emailExtern = "e1@gmail.com";
     public static FacadeQuestion instance = null;
 
     public static FacadeQuestion getInstance(){
+
         if(instance == null){
 
-            instance = new FacadeQuestion();;
+            instance = new FacadeQuestion();
 
         }
 
         return instance;
     }
 
+    public void login(String login){
+
+        if(user == null){
+
+            if(login == null || !login.equals(emailCoordinator)){
+
+                user = new ExternalPublic(R.drawable.emanoel,"emanoel","senha123",emailExtern);
+
+            }else {
+
+                user = new Coordinator(R.drawable.emanoel,"emanoel","senha123",emailCoordinator);
+
+            }
+        }
+
+    }
+
     private  FacadeQuestion(){
 
         position = 0;
 
+    }
+
+    public void logout(){
+        user = null;
+    }
+
+
+    public boolean checkUserCoordinator(){
+        return emailCoordinator.equalsIgnoreCase(user.getEmail());
     }
 
      public void setFragmentQuestionText(Question question){
@@ -74,6 +107,12 @@ public class FacadeQuestion {
               fragmentTransaction.addToBackStack(null);
               fragmentTransaction.replace(R.id.constraintOpenSelection, fragment).commit();
           }
+      }
+
+      public User getUser(){
+
+        return user;
+
       }
 
 }
