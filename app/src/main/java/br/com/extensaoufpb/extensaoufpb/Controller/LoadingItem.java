bf169e4b7ac55581d9ca5f0c0c4196522c855a82
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-10-19 11:27:09 -0300

    create controller class of items adapter

---
+++ app/src/main/java/br/com/extensaoufpb/extensaoufpb/Controller/LoadingItem.java
@@ -0,0 +1,63 @@
+package br.com.extensaoufpb.extensaoufpb.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoadingItem<Item> {
+
+    private List<Item> items;
+    private List<Item> listDate;
+
+    public LoadingItem(List<Item> listItem,List<Item> listDate){
+
+        this.items = listItem;
+        this.listDate = listDate;
+
+    }
+
+    public LoadingItem() {
+
+        this.items = new ArrayList<>();
+        this.listDate = new ArrayList<>();
+
+    }
+
+
+    public List<Item> loadingParticipant(int offset){
+
+        if(items.size() < listDate.size()){
+
+            if((items.size() + offset) > listDate.size()) {
+
+                offset = listDate.size();
+
+            }
+
+            for(int i = items.size(); i < offset; i++){
+
+                items.add(listDate.get(i));
+
+            }
+
+        }
+
+        return items;
+
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Item> getListDate() {
+        return listDate;
+    }
+
+    public void setListDate(List<Item> listDate) {
+        this.listDate = listDate;
+    }
+}
